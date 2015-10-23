b1c9eb2ceaa1e11a5d449d2a4cec1bd5b3f2bbc3
totzYuta <yuta.totz@gmail.com>
2015-10-24 02:06:17 +0900

    Install RSpec files

---
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(
+        :name => "Name",
+        :time => 1
+      ),
+      Task.create!(
+        :name => "Name",
+        :time => 1
+      )
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+  end
+end
