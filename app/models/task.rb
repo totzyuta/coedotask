0452d29d0df996fdf1302890cca0d65ac0682e0d
totzYuta <yuta.totz@gmail.com>
2015-10-24 15:35:35 +0900

    Add Task.calc_total_time

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+  def self.calc_total_time
+    Task.pluck(:time).inject(&:+)
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "class methods" do
+    describe ".calc_total_time" do
+      before do
+        Task.create(name: "Clean Room", time: 10)
+        Task.create(name: "Clean Room", time: 15)
+        Task.create(name: "Clean Room", time: 20)
+      end
+      it "should returnes total time" do
+        expect(Task.calc_total_time).to eq 45
+      end
+    end
+  end
 end
